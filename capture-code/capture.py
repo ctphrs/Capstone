474759b7b02dd9287e80880a362cbeb10d95d303
henry.wilson <henry.wilson.17@cnu.edu>
2020-10-18 17:22:40 -0400

    modified capture code to be a class - will now implement trigger functionality

--- capture-code/capture.py
+++ capture-code/capture.py
@@ -1,47 +1,62 @@
 from picamera import PiCamera
 import time
 
-camera = PiCamera()
-camera.resolution = (768, 768)
-camera.framerate = 24
+class DogCapture:
 
-default_capture_location = '/mnt/exdr'
-default_record_duration = 5 #secondso
+    '''
+    self.camera = None
+    self.vtype = None
+    self.itype = None
+    self.default_capture_location = None
+    self.default_record_duration = None
+    '''
 
-vtype = '.h264'
-itype = '.png'
+    def __init__(self): #in the future load these from a config file
+        self.camera = PiCamera() #initialize camera object
+        self.camera.resolution = (768, 768) #set resolution
+        self.camera.framerate = 24 #set framerate
+        self.vtype = '.h264' #video type
+        self.itype = '.png' #image type
+        self.default_capture_location = '/mnt/exdr'
+        self.default_record_duration = 5 #seconds
 
-def time_naming():
-    now = time.localtime()
-    nowstr = ''+str(now.tm_year)+str(now.tm_mon)+str(now.tm_mday)+'-'+str(now.tm_hour)+str(now.tm_min)+str(now.tm_sec)
-    return nowstr
+    def time_naming(self):
+        now = time.localtime()
+        nowstr = ''+str(now.tm_year)+str(now.tm_mon)+\
+                str(now.tm_mday)+'-'+str(now.tm_hour)+\
+                str(now.tm_min)+str(now.tm_sec)
+        return nowstr
 
 
-def preview_func():
-    camera.start_preview()
-    time.sleep(5)
-    camera.stop_preview()
+    def preview_func(self):
+        self.camera.start_preview()
+        time.sleep(5)
+        self.camera.stop_preview()
+    
+    def still_func(self, capture_location = None):
+        if capture_location is None:
+            capture_location = self.default_capture_location
+        nowstr = self.time_naming()
+        print('Capturing at:', nowstr)
+        self.camera.capture(capture_location + '/' + nowstr + self.itype)
+        print('Done!')
 
-def still_func(capture_location = None):
-    if capture_location is None:
-        capture_location = default_capture_location
-    nowstr = time_naming()
-    print('Capturing at:', nowstr)
-    camera.capture(capture_location + '/' + nowstr + itype)
-    print('Done!')
-
-def record_func(capture_location = None, record_duration = None):
-    if record_duration is None:
-        record_duration = default_record_duration
-    if capture_location is None:
-        capture_location = default_capture_location
-    nowstr = time_naming()
-    print('Recording at:', nowstr)
-    camera.start_recording(capture_location + '/' + nowstr + vtype)
-    time.sleep(record_duration)
-    camera.stop_recording()
-    print('Done!')
+    def record_func(self, capture_location = None, record_duration = None):
+        if record_duration is None:
+            record_duration = self.default_record_duration
+        if capture_location is None:
+            capture_location = self.default_capture_location
+        nowstr = self.time_naming()
+        print('Recording at:', nowstr)
+        self.camera.start_recording(capture_location + '/' + nowstr + self.vtype)
+        time.sleep(record_duration)
+        self.camera.stop_recording()
+        print('Done!')
     
 
 if __name__ == '__main__':
-    record_func()
+    cap = DogCapture()
+    #cap.init()
+    cap.record_func()
+    cap.record_func()
+    cap.still_func()
