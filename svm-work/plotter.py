dbafa4b76be818eb14ea96e98aea74f15cbb196e
Henry Wilson <henry.wilson.17@cnu.edu>
2021-03-12 18:37:17 -0500

    polished plot code to include labels

--- svm-work/plotter.py
+++ svm-work/plotter.py
@@ -3,12 +3,15 @@ import numpy as np
 
 '''
 Plots a joints behavior over time.
-Takes string as input
+Takes movement_string as input
 '''
 def plot_joint(data):
     xaxis = np.linspace(1, np.size(data)//2, num = np.size(data)//2) #Divide by two because of two columns
     xdata = data[:,0]
     ydata = data[:,1]
-    plt.plot(xaxis,xdata,'r-')
-    plt.plot(xaxis,ydata,'g-')
+    plt.plot(xaxis,xdata,'r-',label='X Coordinate')
+    plt.plot(xaxis,ydata,'g-',label='Y Coordinate')
+    plt.ylabel('Delta Position')
+    plt.xlabel('Frame #')
+    plt.legend()
     plt.show()
