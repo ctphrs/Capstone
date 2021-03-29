8873601deeea1c4fcf78807fdf861feb797d0ee8
Henry Wilson <43455296+Henry-Wilson@users.noreply.github.com>
2021-03-28 21:01:05 -0400

    Update delete_newest_file.py
    let's add more comments later

--- delete_newest_file.py
+++ delete_newest_file.py
@@ -5,19 +5,19 @@ import deeplabcut
 
 
 def new_video():
-    video_path = 'home/project_location/videos'
-    list_of_files = glob.glob(video_path + '/*.mp4')  # * means all if need specific format then *.csv
-    latest_file = max(list_of_files, key=os.path.getctime)
+    video_path = '/home/share'
+    list_of_videos = glob.glob(video_path + '/*.mp4')  # * means all if need specific format then *.csv
+    latest_file = max(list_of_videos, key=os.path.getctime)
     print(latest_file)
 
-    config_path = '/home/Capstone/config.yaml'
+    config_path = '/home/CoffeeOverhead-AllenWilson-2021-02-23/config.yaml'
     deeplabcut.analyze_videos(config_path,[latest_file],
         shuffle=1, save_as_csv=True, videotype=.mp4)
 
-    list_of_files = glob.glob(video_path + '/*.csv')
-    csv_destination = '/home/where/do/you/want/it/lol'
+    list_of_csv = glob.glob(video_path + '/*.csv')
+    csv_destination = '/home/afww/CSV'
 
-    for file in list_of_files:
+    for file in list_of_csv:
             original = r'' + file
             filename = file.replace(video_path, '')
             target = r'' + csv_destination + filename
@@ -27,6 +27,6 @@ def new_video():
         os.remove(latest_file)
         print("success")
     else:
-        print("File doesn't exists!")
+        print("File doesn't exist!")
 
     return
